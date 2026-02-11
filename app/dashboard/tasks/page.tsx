bd270d0b96718f0003c9f4cb0ab7de1dd1aac102
zeads <ruri.pelinandang@gmail.com>
2026-02-11 11:13:25 +0700

    fix:_id, title and KanbanClient

--- app/dashboard/tasks/page.tsx
+++ app/dashboard/tasks/page.tsx
@@ -1,12 +1,13 @@
-import { KanbanBoard } from "@/components/tasks/kanban-board";
+// import { KanbanBoard } from "@/components/tasks/kanban-board";
+import { KanbanClient } from "@/components/tasks/kanban-client";
 import { Task } from "@/types/task";
 
 export default async function TasksPage() {
   // Contoh Data (Nantinya ambil dari API)
   const initialTasks: Task[] = [
-    { id: "t-1", content: "Integrasi API Authentication", status: "todo" },
-    { id: "t-2", content: "Design Landing Page", status: "in-progress" },
-    { id: "t-3", content: "Setup Project Next.js", status: "done" },
+    { _id: "t-1", title: "Integrasi API Authentication", status: "todo" },
+    { _id: "t-2", title: "Design Landing Page", status: "in-progress" },
+    { _id: "t-3", title: "Setup Project Next.js", status: "done" },
   ];
 
   return (
@@ -18,7 +19,8 @@ export default async function TasksPage() {
         </p>
       </div>
 
-      <KanbanBoard initialTasks={initialTasks} />
+      {/* <KanbanBoard initialTasks={initialTasks} /> */}
+      <KanbanClient initialTasks={initialTasks} projectId="" />
     </div>
   );
 }
