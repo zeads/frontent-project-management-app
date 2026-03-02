4cff962304f760a40df1301c0aa0e785f403b11c
zeads <ruri.pelinandang@gmail.com>
2026-03-02 17:34:08 +0700

    fix: reload pricing

--- components/layout/Pricing.tsx
+++ components/layout/Pricing.tsx
@@ -29,40 +29,33 @@ export default function Pricing() {
   ];
 
   return (
-    <div id="pricing" className="h-screen flex flex-col gap-5 pt-30">
-      {/* <div className="flex flex-col gap-5 items-center justify-center">
-        <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-          Pricing
-        </h1>
-        <p className="mt-2 text-lg text-slate-700">
-          Choose the plan that&apos;s right for you.
-        </p>
-      </div> */}
-      <div className="flex gap-5 items-center justify-center">
-        <PriceCard
-          forTitle="For Individuals"
-          title="Basic"
-          description="Perfect for Small teams and individuals just starting out with project
+    <div
+      id="pricing"
+      className="h-screen flex gap-5 items-center justify-center"
+    >
+      <PriceCard
+        forTitle="For Individuals"
+        title="Basic"
+        description="Perfect for Small teams and individuals just starting out with project
           management."
-          price="$10"
-          whatIncluded={whatIncludedBasic}
-        />
-        <PriceCard
-          forTitle="For Startups"
-          title="Pro"
-          description="Perfect for Growing teams that need advanced features to boost productivity."
-          price="$25"
-          whatIncluded={whatIncludedPro}
-          mostPopular={true}
-        />
-        <PriceCard
-          forTitle="For Big Companies"
-          title="Enterprise"
-          description="Perfect for Large teams and enterprises with complex project management needs."
-          price="$50"
-          whatIncluded={whatIncludedEnterprise}
-        />
-      </div>
+        price="$10"
+        whatIncluded={whatIncludedBasic}
+      />
+      <PriceCard
+        forTitle="For Startups"
+        title="Pro"
+        description="Perfect for Growing teams that need advanced features to boost productivity."
+        price="$25"
+        whatIncluded={whatIncludedPro}
+        mostPopular={true}
+      />
+      <PriceCard
+        forTitle="For Big Companies"
+        title="Enterprise"
+        description="Perfect for Large teams and enterprises with complex project management needs."
+        price="$50"
+        whatIncluded={whatIncludedEnterprise}
+      />
     </div>
   );
 }
